33a33458084fdf5eb1586e49fbca3061530f3324
w1nd <584202045@qq.com>
2022-02-09 08:37:48 +0800

    认证服务

---
+++ grainmall-auth-server/src/main/java/com/w1nd/grainmall/auth/GrainmallAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.w1nd.grainmall.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrainmallAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrainmallAuthServerApplication.class, args);
+    }
+
+}
